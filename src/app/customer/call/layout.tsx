51dd8a22abc18fe9e55a6d6cb161f4d8dfef73d0
Thirdy Gayares <gayaresthird@gmail.com>
2025-05-18 05:09:45 +0800

    feat: add CallLayout and agent/customer console components for meeting functionality

---
+++ src/app/customer/call/layout.tsx
@@ -0,0 +1,17 @@
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Nexus Agent",
+  description: "Created by Team 9x3",
+};
+
+export default function CallLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+        <>{children}</>
+  );
+}
