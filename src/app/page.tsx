21d2c82b0658cbfa8af04df64f3111b531e9f305
Thirdy Gayares <gayaresthird@gmail.com>
2025-05-17 16:57:06 +0800

    tech: install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="h-screen bg-amber-400 container m-auto flex items-center justify-center">
         Nexus AI
+        <Button variant="default">Click Me</Button>
     </div>
   );
 }
